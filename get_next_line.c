6448bf24760f4c351dd7c39d54667f4430d1b52e
Lea Meubrink <lmeubrin@c3a1c1.42berlin.de>
2024-06-03 19:25:04 +0200

    overwriting previous buffer sadly

--- get_next_line.c
+++ get_next_line.c
@@ -33,23 +33,23 @@ char	*ft_substr(char *src, unsigned int len)
 	return (dest);
 }
 
-/*
-char	*make_rightside(char *buffer, unsigned int len)
-{
-	char	*rightside;
 
-	{
-		rightside = ft_substr(buffer, len);
-		if (!rightside)
-			return (NULL);
-		buffer = ft_substr(rightside, BUFFER_SIZE * 2 + 1);
-		if (!buffer)
-			return (NULL);
-		free(rightside);
-	}
-	return (rightside);
-}
-*/
+// char	*make_rightside(char *buffer, unsigned int len)
+// {
+// 	char	*rightside;
+//
+// 	{
+// 		rightside = ft_substr(buffer, len);
+// 		if (!rightside)
+// 			return (NULL);
+// 		buffer = ft_substr(rightside, BUFFER_SIZE * 2 + 1);
+// 		if (!buffer)
+// 			return (NULL);
+// 		free(rightside);
+// 	}
+// 	return (rightside);
+// }
+
 
 char	*make_line(char *buffer)
 {
@@ -59,7 +59,7 @@ char	*make_line(char *buffer)
 	i = 0;
 	if (!buffer[i])
 		return (ft_substr(buffer, 0));
-	while (buffer[i] && buffer[i] != '\n')
+	while (i + 1 < BUFFER_SIZE && buffer[i] && buffer[i] != '\n')
 		i++;
 	if (!buffer[i] || buffer[i] == '\n')
 	{
@@ -67,8 +67,6 @@ char	*make_line(char *buffer)
 		if (!line)
 			return (free_str(line));
 		ft_strlcpy(buffer, buffer + i + 1, i + 1);
-		//while (i < len)
-		//	buffer[i++] = '\0';
 		return (line);
 	}
 	return (NULL);
@@ -87,13 +85,13 @@ char	*ft_realloc(char *buffer, unsigned int start, unsigned int len)
 		if (!buffer)
 			return (NULL);
 		return (buffer);
-	}		
+	}
 	bufferlen = start; 
 	if (bufferlen < len)
-		init_len = len * 2;
+		init_len = len + BUFFER_SIZE;
 	else
-		init_len = bufferlen * 2;
-	save = ft_substr(buffer + start, init_len - start);
+		init_len = bufferlen + BUFFER_SIZE;
+	save = ft_substr(buffer, BUFFER_SIZE);
 	if (!save)
 		return (NULL);
 	free(buffer);
@@ -105,37 +103,51 @@ char	*ft_realloc(char *buffer, unsigned int start, unsigned int len)
 	return (buffer);
 }
 
+char	*read_into(char *buffer, char *line, int fd, int *readlen)
+{
+	unsigned int	len;
+
+	{
+		len = ft_strlen(buffer);
+		buffer = ft_realloc(buffer, len, BUFFER_SIZE);
+		if (!buffer)
+			return (NULL);
+		*readlen = read(fd, buffer + len, BUFFER_SIZE);
+		if (*readlen < 0)
+			return (free_str(buffer));
+		buffer[len + *readlen] = '\0';
+		line = make_line(buffer);
+		if (line)
+			return (line);
+	}
+	if (*readlen == 0)
+	{
+		line = make_line(buffer);
+		if (line)
+			return (line);
+	}
+	return (NULL);
+}
+
 char	*get_next_line(int fd)
 {
 	char			*line;
 	static char		*buffer;
 	int				readlen;
-	unsigned long	len;
 
 	line = NULL;
+	readlen = 1;
 	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (0);
-	buffer = ft_realloc(buffer, ft_strlen(buffer), BUFFER_SIZE);
-	if (!buffer)
-		return (NULL);
-	len = ft_strlen(buffer);
-	readlen = read(fd, buffer, BUFFER_SIZE);
-	while ((readlen > 0))
+	if (buffer)
 	{
-		buffer = ft_realloc(buffer, ft_strlen(buffer), BUFFER_SIZE);
-		if (!buffer)
-			return (NULL);
-		readlen = read(fd, buffer + readlen, BUFFER_SIZE);
-		if (readlen < 0)
-			return (NULL);
-		buffer[len + readlen] = '\0';
 		line = make_line(buffer);
 		if (line)
 			return (line);
 	}
-	if (readlen == 0)
+	while ((readlen > 0))
 	{
-		line = make_line(buffer);
+		line = read_into(buffer, line, fd, &readlen);
 		if (line)
 			return (line);
 	}
