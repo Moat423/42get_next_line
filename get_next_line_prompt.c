997fbce0b0a329e6732ee73191d49ef5beadfe2f
Moat423 <schluesselfehlt@gmail.com>
2024-12-16 14:29:22 +0100

    ft_gnl with prompt and signal printing suppression

---
+++ get_next_line_prompt.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_prompt.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 13:57:32 by lmeubrin          #+#    #+#             */
+/*   Updated: 2024/12/16 14:26:40 by lmeubrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+#include "get_next_line.h"
+
+void	suppress_cntl_chars(void)
+{
+	struct termios	term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+void	restore_cntl_chars(void)
+{
+	struct termios	term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag |= ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+char	*ft_gnl(int fd, const char *prompt)
+{
+	char	*line;
+
+	if (isatty(STDIN_FILENO))
+		write(STDIN_FILENO, prompt, ft_strlen(prompt));
+	suppress_cntl_chars();
+	line = get_next_line(fd);
+	restore_cntl_chars();
+	return (line);
+}
